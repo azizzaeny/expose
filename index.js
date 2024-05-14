07b19b126a7a60129e3d9f5cbb6708d7021fe7bf
azizzaeny <zie.azlance@gmail.com>
2024-05-14 10:59:27 +0700

    add package and define, evaluate, expose

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+var define = module => Object.assign(global, require(module));
+
+var evaluate= (opt) => {
+  if(!opt){
+    opt = { ctx: global, addCtx: {console, require, module}};
+  }    
+  return (res) => {
+    let vm = require('vm');
+    let context = vm.createContext(opt.ctx);
+    return vm.runInContext(res, Object.assign(context, opt.addCtx));
+  }
+}
+
+var readDir = (dirPath) => fs.readdirSync(dirPath)
+var readFile = (file) => fs.readFileSync(file, 'utf8');
+var isJs =  file => (require('path').extname(file) === '.js');
+var isDir = dirPath => fs.statSync(dirPath).isDirectory();
+
+var expose = (path)=>{
+  if(!isDir(path)) return evaluate(null)(readFile(path));
+  let dir = filter(isJs, readDir(path)) || [];
+  let f = map((e)=> {
+    let loc =path+'/'+e;
+    return readFile(loc);
+  }, dir);
+  return map(evaluate(null), f);
+}
+
+module.exports = { define, evaluate, expose };
